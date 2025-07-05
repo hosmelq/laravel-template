22c198e254cbd30397924622ae45b68482422fe2
Hosmel Quintana <hosmelq@gmail.com>
2025-07-05 15:25:26 -0600

    inertia vue

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{cjs,js,jsx,mjs,ts,tsx}': ['pnpm run fix', 'pnpm run format'],
+  '*.{cjs,js,jsx,mjs,ts,tsx,vue}': ['pnpm run fix', 'pnpm run format'],
   '*.{css,json,md}': 'pnpm run format',
   '!(*.blade).php': ['composer rector -- --dry-run', 'composer pint'],
   '!(*.blade|tests/**).php': ['composer phpstan'],
